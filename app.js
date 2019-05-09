4bb6e9e8ef9f16d803e0f8a0cbd81747109865fb
sebastien85540 <sebastien.reynaud123@orange.fr>
2019-05-09 16:46:46 +0200

    video23

--- app.js
+++ app.js
@@ -56,9 +56,13 @@ app.post("/articles/post", (req, res) => {
     const { image } = req.files
     const uploadFile = path.resolve(__dirname, 'public/articles', image.name);
     image.mv(uploadFile, (error) => {
-        post.create(req.body, (error, post) => {
-            res.redirect("/")
-        })
+        post.create({
+            ...req.body,
+            image: `/articles/${image.name}`
+        },
+            (error, post) => {
+                res.redirect("/")
+            })
         // console.log(req.body);
 
     })
